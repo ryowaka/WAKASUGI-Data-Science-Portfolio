62d3e65f2b0da5a3fc3aadd1d9360917cbe84a42
ryowaka <rwakasug@nd.edu>
2026-09-15 10:13:05 -0400

    week4

--- week3/my_app.py
+++ week3/my_app.py
@@ -7,3 +7,18 @@ if st.button("Click me!"):
     st.write("🎉 You clicked the button! Nice work! 🚀")
 else:
     st.write("Click the button to see what happens...")
+
+import pandas as pd
+
+st.subheader("Exploring Our Dataset")
+
+df = pd.read_csv('data/sample_data.csv')
+
+st.write("Here's our data")
+st.dataframe(df)
+
+city = st.selectbox("Select a city", df["City"].unique())
+st.write(f"People in {city}")
+st.dataframe(df[df["City"] == city])
+
+st.bar_chart(df["Salary"])
